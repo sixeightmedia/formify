8252bac4995c3df34bf4d94a06e316921545d156
Mississippi College <www@mc.edu>
2018-03-20 12:10:32 -0500

    Commerce updates

--- elements/field_types/product/view.php
+++ elements/field_types/product/view.php
@@ -21,13 +21,13 @@
 				<?php echo $symbol; ?><?php echo number_format($field->price,2); ?>
 			</label>
 		</fieldset>
-		<input type="hidden" class="formify-field formify-hidden" id="formify-field-<?php echo $field->ffID; ?>" name="<?php echo $field->ffID; ?>" value="<?php echo $field->qtyEnd; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2); ?>" />
+		<input type="hidden" class="formify-field formify-hidden" id="formify-field-<?php echo $field->ffID; ?>" name="<?php echo $field->ffID; ?>" value="<?php echo $field->qtyEnd; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2,'.',''); ?>" />
 		
 	<?php } elseif (($field->qtyEnd == 1) && ($field->qtyStart == 0)) { //Display regular checkbox: User has an option to purchase only 1 of the product ?>
 		
 		<fieldset>
 			<label class="formify-checkbox-label">
-				<input class="formify-field formify-checkbox" type="checkbox" name="<?php echo $field->ffID;?>" value="<?php echo $field->qtyEnd; ?>" id="formify-field-<?php  echo $field->ffID; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2); ?>" />
+				<input class="formify-field formify-checkbox" type="checkbox" name="<?php echo $field->ffID;?>" value="<?php echo $field->qtyEnd; ?>" id="formify-field-<?php  echo $field->ffID; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2,'.',''); ?>" />
 				<span><i class="fa"></i></span>
 				<?php echo $symbol; ?><?php echo number_format($field->price,2); ?>
 			</label>
@@ -35,7 +35,7 @@
 		
 	<?php } elseif (($field->qtyEnd - $field->qtyStart > 1) && ($field->qtyIncrement > 0)) { //Display dropdown: User selects how many to purchase ?>
 		
-		<select class="formify-field formify-select" id="formify-field-<?php echo $field->ffID; ?>" name="<?php echo $field->ffID; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2); ?>">
+		<select class="formify-field formify-select" id="formify-field-<?php echo $field->ffID; ?>" name="<?php echo $field->ffID; ?>" data-formify-commerce-multiplier="<?php echo number_format($field->price,2,'.',''); ?>">
 			<?php for($i = $field->qtyStart;$i<=$field->qtyEnd;$i+=$field->qtyIncrement) { ?>
 				<option value="<?php echo $i; ?>"><?php echo $i; ?></option>'
 			<?php } ?>
